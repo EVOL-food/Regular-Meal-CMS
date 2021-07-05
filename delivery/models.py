fb569c28fe3a564a75cc243d86f029c29028cc8a
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2021-07-05 14:43:58 +0300

    Delete DeliveryTime, edit DeliveryShedule.

--- delivery/models.py
+++ delivery/models.py
@@ -5,13 +5,10 @@ class DeliveryVendor(models.Model):
     description = models.TextField(max_length=300)
     price_one_delivery = models.DecimalField(default=0.0, max_digits=12, decimal_places=2)
 
-class DeliveryTime(models.Model):
-    datetime = models.DurationField()
 
 class DeliverySchedule(models.Model):
     delivery_vendor = models.ForeignKey(DeliveryVendor, on_delete=models.CASCADE, null=True)
-    delivery_time = models.ManyToManyField(DeliveryTime, on_delete=models.CASCADE, null=True)
-    price_per_day = models.DecimalField(default=0.0, max_digits=12, decimal_places=2)
+    delivery_time = models.DurationField()
 
 
 
