029056ce7c3d65bf3401345efa27bb569e1bc3e4
imgVOID <imgvoid@gmail.com>
2021-07-05 14:14:07 +0300

    menu models tests

--- tests/menu/test_models.py
+++ tests/menu/test_models.py
@@ -2,14 +2,9 @@ from django.test import TestCase
 from menu.models import Menu
 
 
-class AnimalTestCase(TestCase):
+class MenuTestCase(TestCase):
     def setUp(self):
-        Menu.objects.create(name="lion", sound="roar")
-        Menu.objects.create(name="cat", sound="meow")
+        pass
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Menu.objects.get(name="lion")
-        cat = Menu.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+    def test_get(self):
+        pass
