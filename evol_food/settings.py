ef2ec23f77f92cc70516965ad65238fe3f8f9281
Ozerov Alex <sasha.ozerov98@gmail.com>
2021-07-05 14:12:24 +0300

    new

--- evol_food/settings.py
+++ evol_food/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
+    'delivery',
+    'menu',
+    'subscription'
 ]
 
 MIDDLEWARE = [
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subscription(models.Model):
+    days = models.CharField(max_length=50, blank=True)
+    menu = models.ForeignKey(Menu, blank=True, on_delete=models.CASCADE, null=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    delivery_schedule = models.ForeignKey(Delivery, on_delete=models.SET_NULL, null=True)
+
+
+class Order(models.Model):
+    profile = models.ForeignKey(Profile, blank=True, on_delete=models.CASCADE, null=True)
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    data_start = models.DateField(auto_now=True)
+    data_end = models.DateField(auto_now=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    status = models.BooleanField(default=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
