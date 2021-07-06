e816a75930e894e2e27380c61df684d902952dbc
imgVOID <imgvoid@gmail.com>
2021-07-06 20:48:36 +0300

    menu models admin register

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Menu, DailyMeal, Dish, Ingredient, Category
 
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(DailyMeal)
+admin.site.register(Dish)
+admin.site.register(Ingredient)
+admin.site.register(Category)
