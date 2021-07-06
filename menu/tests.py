c937b1da8f2306c27634060005fc873278c44111
imgVOID <imgvoid@gmail.com>
2021-07-06 20:49:52 +0300

    menu tests.py

--- menu/tests.py
+++ menu/tests.py
@@ -1,3 +1,22 @@
+import json
 from django.test import TestCase
+from menu.models import Menu, Dish, Category, Ingredient
 
-# Create your tests here.
+
+# Дамп данных с поддержкой Юникода:
+# python -Xutf8 ./manage.py dumpdata menu > dish.json
+
+
+class DishTestCase(TestCase):
+    fixtures = ['dish.json']
+
+    def test_get(self):
+        dish = Dish.objects.get(title="Супертост с авокадо")
+        self.assertEqual(dish.title, "Супертост с авокадо")
+        self.assertEqual(dish.calories, 713)
+        self.assertEqual(dish.meal_of_the_day, 2)
+        ingredients = ['Оливковое масло', 'Авокадо', 'Вяленые помидоры', 'Лимонный сок',
+                       'Молотый сушеный чеснок', 'Ржаной хлеб', 'Перепелиное яйцо',
+                       'Редис', 'Черные кунжутные семечки', 'Соль', 'Черный перец']
+        self.assertEqual([str(ingredient) for ingredient in dish.ingredients.all()],
+                         ingredients)
