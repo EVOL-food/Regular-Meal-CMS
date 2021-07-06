582e718c5ece8013a260ea7faf9b565286e78e55
alex-ozerov <82468378+alex-ozerov@users.noreply.github.com>
2021-07-06 14:50:16 +0300

    test and pre_save subscription

--- tests/menu/test_models.py
+++ tests/menu/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from menu.models import Menu
+from subscription.models import Subscription, Order
 from delivery.models import DeliveryVendor, DeliverySchedule
 
 
@@ -10,6 +11,22 @@ class MenuTestCase(TestCase):
     def test_get(self):
         pass
 
+
+class SubscriptionTestCase(TestCase):
+    def setUp(self):
+        Subscription.objects.create(days='30', menu="Body", price='6005', delivery_schedule="11.30")
+
+    def test_get(self):
+        subscription = Subscription.objects.get(days='30')
+        self.assertEqual(subscription.days, '30')
+
+
+class OrderTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get(self):
+        pass
 class DeliveryVendorTestCase(TestCase):
     def setUp(self):
         DeliveryVendor.objects.create(title='monday', description='address',
