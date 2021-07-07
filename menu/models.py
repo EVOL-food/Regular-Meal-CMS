bedeffb43aaad71db8b7f7ecde790e4ca6ea9190
imgVOID <imgvoid@gmail.com>
2021-07-07 12:53:04 +0300

    menu models.py fixes

--- menu/models.py
+++ menu/models.py
@@ -8,12 +8,12 @@ from django.utils.text import slugify
 class Category(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=1000)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(max_length=30, unique=True, default="")
 
 
 class Ingredient(models.Model):
     title = models.CharField(max_length=30, unique=True)
-    slug = models.SlugField(max_length=30, blank=True, null=True)
+    slug = models.SlugField(max_length=30, unique=True, default="")
 
     def __str__(self):
         return self.title
@@ -24,7 +24,7 @@ class Dish(models.Model):
     calories = models.PositiveIntegerField(default=0)
     meal_of_the_day = models.PositiveIntegerField(
         choices=[(i, str(i)) for i in range(1, 6)])
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(max_length=60, unique=True, default="")
 
     ingredients = models.ManyToManyField(Ingredient, blank=True)
 
@@ -34,38 +34,57 @@ class Dish(models.Model):
 
 class DailyMeal(models.Model):
     title = models.CharField(default="", max_length=100)
-    dishes = models.ManyToManyField(Dish, blank=True, related_name='daily_meal')
+
+    dish_1 = models.ForeignKey(Dish, blank=True, null=True, on_delete=models.SET_NULL,
+                               related_name='breakfast')
+    dish_2 = models.ForeignKey(Dish, blank=True, null=True, on_delete=models.SET_NULL,
+                               related_name='brunch')
+    dish_3 = models.ForeignKey(Dish, blank=True, null=True, on_delete=models.SET_NULL,
+                               related_name='lunch')
+    dish_4 = models.ForeignKey(Dish, blank=True, null=True, on_delete=models.SET_NULL,
+                               related_name='dinner')
+    dish_5 = models.ForeignKey(Dish, blank=True, null=True, on_delete=models.SET_NULL,
+                               related_name='supper')
+
     calories = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
 
 
 class Menu(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=1000)
     calories_daily = models.PositiveIntegerField(default=0)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(max_length=30, unique=True, default="")
 
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True, blank=True)
 
     price_custom = models.BooleanField(default=False)
-    price_daily = models.DecimalField(default=0, max_digits=5, decimal_places=4,)
+    price_daily = models.DecimalField(default=0, max_digits=5, decimal_places=4)
     price_weekly = models.DecimalField(default=0, max_digits=5, decimal_places=4)
     price_monthly = models.DecimalField(default=0, max_digits=5, decimal_places=4)
 
-    day_1 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_1")
-    day_2 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_2")
-    day_3 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_3")
-    day_4 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_4")
-    day_5 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_5")
-    day_6 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_6")
-    day_7 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="day_7")
+    day_1 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="monday")
+    day_2 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="tuesday")
+    day_3 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="wednesday")
+    day_4 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="thursday")
+    day_5 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="friday")
+    day_6 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="saturday")
+    day_7 = models.ForeignKey(DailyMeal, on_delete=models.CASCADE, related_name="sunday")
 
 
 @receiver(pre_save, sender=Dish)
-def delivery_vendor_pre_save(sender, instance, *args, **kwargs):
+def pre_save_dish(sender, instance, *args, **kwargs):
     instance.slug = slugify(unidecode.unidecode(instance.title))
 
 
 @receiver(pre_save, sender=Ingredient)
+def pre_save_ingredient(sender, instance, *args, **kwargs):
+    instance.slug = slugify(unidecode.unidecode(instance.title))
+
+
+@receiver(pre_save, sender=DailyMeal)
 def delivery_vendor_pre_save(sender, instance, *args, **kwargs):
     instance.slug = slugify(unidecode.unidecode(instance.title))
