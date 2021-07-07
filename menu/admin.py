e64e706b1af8c7108a38dc65730a2ec951e92eb0
alex-ozerov <82468378+alex-ozerov@users.noreply.github.com>
2021-07-07 14:09:55 +0300

    json

--- menu/admin.py
+++ menu/admin.py
@@ -2,8 +2,22 @@ from django.contrib import admin
 from .models import Menu, DailyMeal, Dish, Ingredient, Category
 
 # Register your models here.
-admin.site.register(Menu)
-admin.site.register(DailyMeal)
-admin.site.register(Dish)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'calories_daily')
+
+
+class DailyMealAdmin(admin.ModelAdmin):
+    list_display = ('title', 'calories')
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('title', 'calories', 'meal_of_the_day')
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(DailyMeal, DailyMealAdmin)
+admin.site.register(Dish, DishAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Category)
