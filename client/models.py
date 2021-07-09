5b9bf914fd85988f62f32643f02ca4ec0edd8685
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2021-07-09 14:28:05 +0300

    complete client: models, admin

--- client/models.py
+++ client/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from uuid import uuid4
+
 # Create your models here.
 
 class Client(models.Model):
@@ -9,10 +11,16 @@ class Client(models.Model):
         ('Женщина', 'Женщина'),
         ('Иначе', 'Иначе'),
     )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Номер телефона должен быть в формате '+999999999'")
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    phone = models.CharField(blank=True, max_length=20)
-    created_at = models.DateTimeField(auto_now=True)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17)
+    address = models.CharField(max_length=300)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
