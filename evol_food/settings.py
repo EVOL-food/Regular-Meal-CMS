fe8d6c68399a36144b08845ace52d3512c4bef38
imgVOID <imgvoid@gmail.com>
2021-07-09 12:30:56 +0300

    adminlte3 support

--- evol_food/settings.py
+++ evol_food/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlte3',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
