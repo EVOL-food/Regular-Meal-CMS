fa7d97931ee67f2ea170423c14610d99d434cc77
alex-ozerov <82468378+alex-ozerov@users.noreply.github.com>
2021-07-16 12:30:47 +0300

    client views

--- client/models.py
+++ client/models.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 
 # Create your models here.
 
+
 class Client(models.Model):
     GENDER_CHOICES = (
         ('Мужчина', 'Мужчина'),
@@ -16,7 +17,8 @@ class Client(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Номер телефона должен быть в формате '+999999999'")
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                unique=True, primary_key=True, blank=False)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     phone_number = models.CharField(validators=[phone_regex], max_length=17)
