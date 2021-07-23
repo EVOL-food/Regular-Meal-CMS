926fc83d0bce11cde0355915a44fe27d148b31ec
imgVOID <imgvoid@gmail.com>
2021-07-23 22:19:09 +0300

    subscription tests.py

--- subscription/fixtures/model_recipes.py
+++ subscription/fixtures/model_recipes.py
@@ -1,9 +1,10 @@
 from datetime import date
-from model_bakery.recipe import Recipe, foreign_key
+from model_mommy.recipe import Recipe, foreign_key, related
+from model_mommy import seq
+from menu.fixtures.model_recipes import menu
+from delivery.fixtures.model_recipes import delivery_schedule
+from client.fixtures.model_recipes import profile
 from subscription.models import Subscription, Order
-from menu.fixtures.baker_recipes import menu
-from delivery.fixtures.baker_recipes import delivery_schedule
-from client.fixtures.baker_recipes import client
 
 subscription = Recipe(
     Subscription,
@@ -15,7 +16,7 @@ subscription = Recipe(
 
 order = Recipe(
     Order,
-    profile=foreign_key(client),
+    profile=foreign_key(profile),
     subscription=foreign_key(subscription),
     data_start=date(year=2020, month=1, day=1),
 )
--- subscription/tests.py
+++ subscription/tests.py
@@ -1,15 +1,15 @@
 import datetime
 from django.test import TestCase
-from model_bakery import baker
-from subscription.models import Subscription, Order
 from delivery.models import DeliverySchedule
 from menu.models import Menu
 from client.models import Profile
+from subscription.models import Subscription
+from subscription.fixtures import model_recipes
 
 
 class SubscriptionTestCase(TestCase):
     def setUp(self) -> None:
-        self.subscription = baker.make_recipe('subscription.fixtures.subscription')
+        self.subscription = model_recipes.subscription.make()
 
     def test_field(self):
         self.assertEqual(self.subscription.days, 28)
@@ -19,9 +19,9 @@ class SubscriptionTestCase(TestCase):
         self.assertIsInstance(self.subscription.menu, Menu)
         self.assertIsInstance(self.subscription.delivery_schedule,
                               DeliverySchedule)
-        self.assertEqual(self.subscription.menu.title, 'Test Menu')
+        self.assertEqual(self.subscription.menu.title, 'TITLE en121')
         self.assertEqual(self.subscription.delivery_schedule.delivery_vendor.title,
-                         'Test Delivery Vendor')
+                         'Title 1')
 
     def test_pre_save(self):
         self.assertEqual(self.subscription.price_menu,
@@ -45,16 +45,14 @@ class SubscriptionTestCase(TestCase):
 
 class OrderTestCase(TestCase):
     def setUp(self) -> None:
-        self.order = baker.make_recipe('subscription.fixtures.order')
+        self.order = model_recipes.order.make()
 
     def test_field(self):
         self.assertEqual(self.order.data_start, datetime.date(year=2020, month=1, day=1))
         self.assertTrue(self.order.status, 1)
 
     def test_foreign_key(self):
-        self.assertIsInstance(self.order.profile, Profile)
-        self.assertIsInstance(self.order.subscription, Subscription)
-        self.assertEqual(self.order.profile.first_name, 'Mario')
+        self.assertEqual(self.order.profile.first_name, 'Name1')
         self.assertEqual(self.order.subscription.days, 28)
 
     def test_pre_save(self):
