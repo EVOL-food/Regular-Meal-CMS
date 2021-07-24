59131033cf85d4928e4739a6a14a2ed6d61c1524
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2021-07-24 17:13:51 +0300

    add MenuTitleSearchFilter, DishSearchFilter, AverageCalorieSearchFilter

--- menu/views.py
+++ menu/views.py
@@ -53,30 +53,49 @@ class MenuRetrieveView(RetrieveAPIView):
         return self.retrieve(request, *args, **kwargs)
 
 
+class MenuTitleSearchFilter(SearchFilter):
+    def get_search_fields(self, view, request):
+        if request.query_params.get('title'):
+            return ['title']
+        return super(MenuTitleSearchFilter, self).get_search_fields(view, request)
+
+
+class DishSearchFilter(SearchFilter):
+    def get_search_fields(self, view, request):
+        if request.query_params.get('dish'):
+            return ['day_1__dish_1__title', 'day_1__dish_2__title', 'day_1__dish_3__title',
+                    'day_1__dish_4__title', 'day_1__dish_5__title',
+                    'day_2__dish_1__title', 'day_2__dish_2__title', 'day_2__dish_3__title',
+                    'day_2__dish_4__title', 'day_2__dish_5__title',
+                    'day_3__dish_1__title', 'day_3__dish_2__title', 'day_3__dish_3__title',
+                    'day_3__dish_4__title', 'day_3__dish_5__title',
+                    'day_4__dish_1__title', 'day_4__dish_2__title', 'day_4__dish_3__title',
+                    'day_4__dish_4__title', 'day_4__dish_5__title',
+                    'day_5__dish_1__title', 'day_5__dish_2__title', 'day_5__dish_3__title',
+                    'day_5__dish_4__title', 'day_5__dish_5__title',
+                    'day_6__dish_1__title', 'day_6__dish_2__title', 'day_6__dish_3__title',
+                    'day_6__dish_4__title', 'day_6__dish_5__title',
+                    'day_7__dish_1__title', 'day_7__dish_2__title', 'day_7__dish_3__title',
+                    'day_7__dish_4__title', 'day_7__dish_5__title',]
+        return super(DishSearchFilter, self).get_search_fields(view, request)
+
+
+class AverageCalorieSearchFilter(SearchFilter):
+    def get_search_fields(self, view, request):
+        if request.query_params.get('calories'):
+            return ['day_1__calories', 'day_2__calories', 'day_3__calories',
+                    'day_4__calories', 'day_5__calories', 'day_6__calories',
+                    'day_7__calories',]
+        return super(AverageCalorieSearchFilter, self).get_search_fields(view, request)
+
+
 class SearchDetailView(ListAPIView):
     serializer_class = MenuSerializerDetail
     queryset = Menu.objects.all()
     permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['slug',
-                     'day_1__dish_1__slug', 'day_1__dish_2__slug', 'day_1__dish_3__slug',
-                     'day_1__dish_4__slug', 'day_1__dish_5__slug',
-                     'day_2__dish_1__slug', 'day_2__dish_2__slug', 'day_2__dish_3__slug',
-                     'day_2__dish_4__slug', 'day_2__dish_5__slug',
-                     'day_3__dish_1__slug', 'day_3__dish_2__slug', 'day_3__dish_3__slug',
-                     'day_3__dish_4__slug', 'day_3__dish_5__slug',
-                     'day_4__dish_1__slug', 'day_4__dish_2__slug', 'day_4__dish_3__slug',
-                     'day_4__dish_4__slug', 'day_4__dish_5__slug',
-                     'day_5__dish_1__slug', 'day_5__dish_2__slug', 'day_5__dish_3__slug',
-                     'day_5__dish_4__slug', 'day_5__dish_5__slug',
-                     'day_6__dish_1__slug', 'day_6__dish_2__slug', 'day_6__dish_3__slug',
-                     'day_6__dish_4__slug', 'day_6__dish_5__slug',
-                     'day_7__dish_1__slug', 'day_7__dish_2__slug', 'day_7__dish_3__slug',
-                     'day_7__dish_4__slug', 'day_7__dish_5__slug',
-                     'day_1__calories', 'day_2__calories', 'day_3__calories',
-                     'day_4__calories', 'day_5__calories', 'day_6__calories',
-                     'day_7__calories',
-                     ]
+    filter_backends = [MenuTitleSearchFilter, DishSearchFilter,
+                       AverageCalorieSearchFilter]
+
 
     def get(self, request, *args, **kwargs):
         language = kwargs["language"]
