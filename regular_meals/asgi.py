c5284ec24e5f73c59bfabbd89fac9141b19c3b69
imgVOID <imgvoid@gmail.com>
2021-09-15 01:49:13 +0300

    project rename

--- regular_meals/asgi.py
+++ regular_meals/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for evol_food project.
+ASGI config for regular_meals project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evol_food.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'regular_meals.settings')
 
 application = get_asgi_application()
--- regular_meals/urls.py
+++ regular_meals/urls.py
@@ -1,4 +1,4 @@
-"""evol_food URL Configuration
+"""regular_meals URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
